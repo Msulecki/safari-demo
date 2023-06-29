555e4db3d3d342a915a64dfb2b0ff3929f45c1e6
Msulecki <Msulecki@users.noreply.github.com>
2023-06-29 08:34:59 +0000

    Deploying to master from @ Msulecki/safari-demo@6847c0f70b0b1db633ad6c10230ade9ea2c3666e 🚀

--- src/components/TransitionResize/TransitionResize.stories.tsx
+++
@@ -1,9 +0,0 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import TransitionResize from '.';
-
-const meta: Meta<typeof TransitionResize> = { component: TransitionResize,parameters:{ layout:'fullscreen'} };
-export default meta;
-
-type Story = StoryObj<typeof TransitionResize>;
-
-export const Primary: Story = { args: { primary: true } };
